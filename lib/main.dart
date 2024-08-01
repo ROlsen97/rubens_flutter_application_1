39f0883a60d0a4401da4d70eacd51b2ce35384c5
ROlsen97 <100783426+ROlsen97@users.noreply.github.com>
2024-08-01 19:47:14 +0200

    Created countdown timer

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'dart:async';
 
 void main() {
   runApp(MyApp());
@@ -28,7 +29,7 @@ class MyApp extends StatelessWidget {
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
 
-  void getNext(){
+  void getNext() {
     current = WordPair.random();
     notifyListeners();
   }
@@ -45,14 +46,16 @@ class MyHomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            BigCard(pair: pair),
-            SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () {
-                appState.getNext();
-              },
-              child: Text('Next'),
-            ),
+            // BigCard(pair: pair),
+            // SizedBox(height: 10),
+            // ElevatedButton(
+            //   onPressed: () {
+            //     appState.getNext();
+            //   },
+            //   child: Text('Next'),
+            // ),
+            // SizedBox(height: 20),
+            CountdownTimer()
           ],
         ),
       ),
@@ -83,8 +86,218 @@ class BigCard extends StatelessWidget {
           pair.asLowerCase, 
           style: style,
           semanticsLabel: pair.asPascalCase,
-          ),
+        ),
       ),
+    );
+  }
+}
+
+class CountdownTimer extends StatefulWidget {
+  @override
+  // ignore: library_private_types_in_public_api
+  _CountdownTimerState createState() => _CountdownTimerState();
+}
+
+class _CountdownTimerState extends State<CountdownTimer> {
+  Timer? _timer;
+  int _start = 0;
+  bool _isRunning = false;
+
+  void startTimer() {
+    if (_timer != null) {
+      _timer?.cancel();
+    }
+    setState(() {
+      _isRunning = true;
+    });
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        if (_start > 0) {
+          _start--;
+        } else {
+          _isRunning = false;
+          _timer?.cancel();
+          showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+              title: Text('Time\'s up!'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('OK'),
+                ),
+              ],
+            ),
+          );
+        }
+      });
+    });
+  }
+
+  void stopTimer() {
+    if (_timer != null) {
+      _timer?.cancel();
+    }
+    setState(() {
+      _isRunning = false;
+    });
+  }
+
+  void resetTimer() {
+    if (_timer != null) {
+      _timer?.cancel();
+    }
+    setState(() {
+      _start = 0;
+      _isRunning = false;
+    });
+  }
+
+  void setTime(int hours, int minutes, int seconds) {
+    setState(() {
+      _start = hours * 3600 + minutes * 60 + seconds;
+    });
+  }
+
+  void setHours() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        TextEditingController _controller = TextEditingController();
+        return AlertDialog(
+          title: Text('Set Hours'),
+          content: TextField(
+            controller: _controller,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(hintText: "Enter hours"),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('OK'),
+              onPressed: () {
+                setTime(int.parse(_controller.text), _minutes(), _seconds());
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void setMinutes() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        TextEditingController _controller = TextEditingController();
+        return AlertDialog(
+          title: Text('Set Minutes'),
+          content: TextField(
+            controller: _controller,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(hintText: "Enter minutes"),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('OK'),
+              onPressed: () {
+                setTime(_hours(), int.parse(_controller.text), _seconds());
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void setSeconds() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        TextEditingController _controller = TextEditingController();
+        return AlertDialog(
+          title: Text('Set Seconds'),
+          content: TextField(
+            controller: _controller,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(hintText: "Enter seconds"),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('OK'),
+              onPressed: () {
+                setTime(_hours(), _minutes(), int.parse(_controller.text));
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  int _hours() {
+    return _start ~/ 3600;
+  }
+
+  int _minutes() {
+    return (_start % 3600) ~/ 60;
+  }
+
+  int _seconds() {
+    return _start % 60;
+  }
+  @override
+  void dispose() {
+    if (_timer != null) {
+      _timer?.cancel();
+    }
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        GestureDetector(
+          onTap: setHours,
+          child: Text(
+            '${_hours().toString().padLeft(2, '0')}',
+            style: TextStyle(fontSize: 48),
+          ),
+        ),
+        GestureDetector(
+          onTap: setMinutes,
+          child: Text(
+            '${_minutes().toString().padLeft(2, '0')}',
+            style: TextStyle(fontSize: 48),
+          ),
+        ),
+        GestureDetector(
+          onTap: setSeconds,
+          child: Text(
+            '${_seconds().toString().padLeft(2, '0')}',
+            style: TextStyle(fontSize: 48),
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: _isRunning ? stopTimer : startTimer,
+              child: Text(_isRunning ? 'Stop' : 'Start'),
+            ),
+            SizedBox(width: 10),
+            ElevatedButton(
+              onPressed: resetTimer,
+              child: Text('Reset'),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
